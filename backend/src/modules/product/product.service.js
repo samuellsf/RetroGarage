2992596cf4c2a6faf901fa843e174a23c27758a1
Alvaro de Sousa <alvarosousa0003@gmail.com>
2026-05-27 21:17:40 -0300

    restante das funcoes do service

--- backend/src/modules/product/product.service.js
+++ backend/src/modules/product/product.service.js
@@ -2,4 +2,20 @@ import * as model from './product.model.js';
 
 export function getProducts() {
   return model.findAll();
-}
+};
+
+export function getProductById(id) {
+  return model.findById(id);
+};
+
+export function getProdutsByCategory(id) {
+  return model.findByCategory(id);
+};
+
+export function createProduct(data) {
+  return model.create(data);
+};
+
+export function deleteProduct(id) {
+  return model.remove(id);
+};
