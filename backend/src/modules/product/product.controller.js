4ca25af05e228ec46afaf244a8d7831ec8db1cb5
Alvaro de Sousa <alvarosousa0003@gmail.com>
2026-05-30 15:19:39 -0300

    finalizado o modulo product da api

--- backend/src/modules/product/product.controller.js
+++ backend/src/modules/product/product.controller.js
@@ -10,4 +10,38 @@ export async function getProducts(req, res) {
   const products = await service.getProducts();
 
   res.json(products);
-}
+};
+
+export async function getProductById(req, res) {
+  const product = await service.getProductById(req.params.id);
+
+  if(!product) {
+    return res.status(404).json({
+      error: 'Produto nao encontrado',
+    });
+  };
+
+  res.json(product);
+};
+
+export async function getProductBycategory(req, res) {
+  const product = await service.getProdutsByCategory(req.params.id);
+
+  res.json(product);
+};
+
+export async function createProduct(req, res) {
+  const result = await service.createProduct(req.body);
+
+  res.status(201).json({
+    message: 'Produto criado',
+  });
+};
+
+export async function deleteProduct(req, res) {
+  await service.deleteProduct(req.params.id);
+
+  res.json({
+    message: 'Produto removido'
+  });
+};
--- backend/src/modules/product/product.routes.js
+++ backend/src/modules/product/product.routes.js
@@ -1,8 +1,18 @@
 import express from 'express';
-import { getProducts } from './product.controller.js';
+import {
+    getProducts,
+    getProductById,
+    getProductBycategory,
+    createProduct,
+    deleteProduct
+} from './product.controller.js';
 
 const router = express.Router();
 
 router.get('/', getProducts);
+router.get('/category/:id', getProductBycategory);
+router.get('/:id', getProductById);
+router.post('/', createProduct);
+router.delete('/:id', deleteProduct);
 
 export default router;
